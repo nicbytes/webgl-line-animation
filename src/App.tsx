8548618383c9f832fa58b04beba912e8a2e4094a
Nicholas Buckeridge <bucknich@gmail.com>
2019-07-31 10:34:10 +1000

    🎨We have colours!

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div style={{width:200, height:200, backgroundColor: 'red' }} />
         <LineAnimationCanvas />
       </header>
     </div>
--- src/LineAnimationCanvas.tsx
+++ src/LineAnimationCanvas.tsx
@@ -1,71 +1,91 @@
-import React, { ReactPropTypes } from 'react'
+import React from 'react'
 import { mat4 } from 'gl-matrix'
 
 const vsSource = `
 attribute vec4 aVertexPosition;
+attribute vec4 aVertexColor;
 
-uniform mat4 uModelViewMatrix;
-uniform mat4 uProjectionMatrix;
+uniform mat4 uTranslationMatrix;
+
+varying lowp vec4 vColor;
 
 void main() {
-  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  gl_Position = uTranslationMatrix * aVertexPosition;
+  vColor = aVertexColor;
 }
 `
 
 const fsSource = `
+varying lowp vec4 vColor;
 void main() {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  // gl_FragColor = vec4(1.0,1.0,1.0,1.0);
+  gl_FragColor = vColor;
 }
 `
 
 interface ProgramInfo {
   program: WebGLProgram
   attribLocations: {
-    vertexPosition: number
+    vertexPosition: number,
+    vertexColor: number
   }
   uniformLocations: {
-    projectionMatrix: WebGLUniformLocation | null
-    modelViewMatrix: WebGLUniformLocation | null
+    translationMatrix: WebGLUniformLocation | null
   }
 }
 
-class TitleCanvas extends React.Component {
+interface PropTypes {
+    children?: React.ReactNode
+    width?: string | number | undefined
+    height?: string | number | undefined
+    center?: boolean
+    onError?: Function
+  }
+
+class TitleCanvas extends React.Component<PropTypes> {
   state = {
     t: 0
   }
   private canvasRef = React.createRef<HTMLCanvasElement>()
+  private errorHandler: Function;
 
-  constructor(props: ReactPropTypes) {
+  constructor(props: PropTypes) {
     super(props)
+    if(props.onError) {
+      this.errorHandler = props.onError
+    } else {
+      this.errorHandler = (e:string) => alert(e)
+    }
   }
 
   componentDidMount() {
-    this.drawCanvas()
+    this.createCanvas()
   }
 
   componentDidUpdate() {
-    this.drawCanvas()
+    this.createCanvas()
   }
 
-  drawCanvas() {
+  /**
+   * Creates a new Canvas instance.
+   */
+  createCanvas() {
     const gl = this.canvasRef.current!.getContext('webgl')
     if (gl === null) {
-      alert('Unable to initialize WebGL. Your browser or machine may not support it.')
+      this.errorHandler('Unable to initialize WebGL. Your browser or machine may not support it.')
       return
     }
-    gl.clearColor(0, 0, 0, 1)
-    gl.clear(gl.COLOR_BUFFER_BIT)
 
     const shaderProgram = this.initShaderProgram(gl, vsSource, fsSource)!
 
     const programInfo = {
       program: shaderProgram,
       attribLocations: {
-        vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition')
+        vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+        vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
       },
       uniformLocations: {
-        projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-        modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
+        translationMatrix: gl.getUniformLocation(shaderProgram, 'uTranslationMatrix'),
       }
     }
 
@@ -74,47 +94,31 @@ class TitleCanvas extends React.Component {
     this.drawScene(gl, programInfo, buffers)
   }
 
-  private drawScene(gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers: { positions: WebGLBuffer }) {
+  private drawScene(gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers: { position: WebGLBuffer, color: WebGLBuffer }) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0) // Clear to black, fully opaque
     gl.clearDepth(1.0) // Clear everything
-    gl.enable(gl.DEPTH_TEST) // Enable depth testing
-    gl.depthFunc(gl.LEQUAL) // Near things obscure far things
-
+    // gl.enable(gl.DEPTH_TEST) // Enable depth testing
+    // gl.depthFunc(gl.LEQUAL) // Near things obscure far things
     // clear the canvas before we start drawing on it
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
-
-    const fieldOfView = (45 * Math.PI) / 180
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight
-    const zNear = 0.1
-    const zFar = 100.0
-    const projectionMatrix = mat4.create()
-
-    mat4.perspective(
-      projectionMatrix, // first arg is destination to receive the result
-      fieldOfView,
-      aspect,
-      zNear,
-      zFar
+    const w = gl.canvas.clientWidth
+    const h = gl.canvas.clientHeight
+    
+    const translationMatrix = mat4.create()
+    mat4.scale(
+        translationMatrix,
+        translationMatrix,
+        [ 2.0/w, 2.0/h, 1.0 ]
     )
+    if (!(this.props.center)) {
+        mat4.translate(
+            translationMatrix,
+            translationMatrix,
+            [ -w/2.0, -h/2.0, 0 ]
+        )
+    }
 
-    // set the view (drawing position?) to the "identity" (center of the screen)
-    const modelViewMatrix = mat4.create()
-
-    mat4.translate(
-      modelViewMatrix, // destination matrix
-      modelViewMatrix, // matrix to translate
-      [0.0, 0.0, -5.0] // amount to translate
-    )
-
-    // Tell WebGL how to pull out the positions from the position buffer
-    // into the vertexBuffer attribute
     {
       const numComponents = 2
       const type = gl.FLOAT
@@ -122,24 +126,42 @@ class TitleCanvas extends React.Component {
       const stride = 0
 
       const offset = 0
-      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.positions)
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position)
       gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, numComponents, type, normalize, stride, offset)
       gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition)
-
-      gl.useProgram(programInfo.program)
-      gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix)
-      gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix)
-
-      {
-        const offset = 0
-        const vertexCount = 4
-        gl.drawArrays(gl.TRIANGLE_FAN, offset, vertexCount)
-        // gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount)
-      }
+    }
+    // Tell WebGL how to pull out the colors from the color buffer
+    // into the vertexColor attribute.
+    {
+        const numComponents = 4;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexColor,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(
+            programInfo.attribLocations.vertexColor);
+    }
+    gl.useProgram(programInfo.program)
+    gl.uniformMatrix4fv(programInfo.uniformLocations.translationMatrix, false, translationMatrix)
+    {
+      const offset = 0
+      const vertexCount = 4
+      // gl.drawArrays(gl.TRIANGLE_FAN, offset, vertexCount)
+      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount)
     }
   }
 
-  private initBuffers(gl: WebGLRenderingContext): { positions: WebGLBuffer } {
+  private initBuffers(gl: WebGLRenderingContext): { position: WebGLBuffer, color: WebGLBuffer } {
+
+
     // Create a buffer for the square's positions.
 
     const positionBuffer = gl.createBuffer()!
@@ -148,14 +170,28 @@ class TitleCanvas extends React.Component {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
     // Now create an array of positions for the square
-    const positions = [-1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0]
+    const positions = [-100.0, 100.0, 100.0, 100.0, -100.0, -100.0, 100.0, -100.0]
+    // const positions = [-0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, -0.5]
+    // const positions = [20,20,20,120,120,120,120,20]
 
     // Now pass the list of positions into WebGL to build the shape.
     // We do this by creating a Float32Array from the JavaScript array,
     // then use it to fill the current buffer.
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
 
-    return { positions: positionBuffer }
+
+    const colors = [
+        1.0,  1.0,  1.0,  1.0,    // white
+        1.0,  0.0,  0.0,  1.0,    // red
+        0.0,  1.0,  0.0,  1.0,    // green
+        0.0,  0.0,  1.0,  1.0,    // blue
+      ];
+
+    const colorBuffer = gl.createBuffer()!;
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    return { position: positionBuffer, color: colorBuffer }
   }
 
   private initShaderProgram(gl: WebGLRenderingContext, vsSrc: string, fsSrc: string): WebGLProgram | null {
@@ -170,7 +206,7 @@ class TitleCanvas extends React.Component {
 
     // If creating the shader program failed, alert
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-      alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram))
+      this.errorHandler('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram))
       return null
     }
     return shaderProgram
@@ -184,7 +220,7 @@ class TitleCanvas extends React.Component {
     gl.compileShader(shader)
     // check if it compiled successfully
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      alert('An error occurred compiling the shader: ' + gl.getShaderInfoLog(shader))
+      this.errorHandler('An error occurred compiling the shader: ' + gl.getShaderInfoLog(shader))
       gl.deleteShader(shader)
       return null
     }
@@ -193,11 +229,11 @@ class TitleCanvas extends React.Component {
   }
 
   render() {
-    return <canvas ref={this.canvasRef} width={360} height={240}></canvas>
+    return <canvas ref={this.canvasRef} width={720} height={250}></canvas>
   }
 }
 
 
-const IndexPage = () => (<TitleCanvas />)
+const IndexPage = () => (<TitleCanvas center />)
 
 export default IndexPage
